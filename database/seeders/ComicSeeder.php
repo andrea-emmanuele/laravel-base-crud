7fd90e4fda394843936af5d396d098353e4ee9fa
andrea-emmanuele <contact@andreaemmanuele.it>
2021-05-20 15:55:17 +0200

    Completed

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -13,6 +15,18 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+        }
     }
 }
